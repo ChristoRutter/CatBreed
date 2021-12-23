5d96479efcbf63c800311e5982ffd7c308c8bcd1
ChristoRutter <92889523+ChristoRutter@users.noreply.github.com>
2021-12-23 09:29:28 -0500

    added two cat breeds

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 
 
+app.get("/api/catBreed", (request, response) => {
+    response.json({
+        
+            domesticShortHair:{
+                weight: "6-16 lbs",
+                lifespan: "15-20 years"
+            },
+            maineCoon:{
+                weight: "8-25 lbs",
+                lifespan: "9-13 years"
+            }
+        
+    })
+})
 
-app.listen(8000, ()=> console.log("connected to port 8000"))
+
+app.listen(8000, ()=> console.log('connected to port 8000'));
