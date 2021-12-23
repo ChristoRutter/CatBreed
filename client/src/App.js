a0ddca52b377272b880850f59dc8f05e2722068a
ChristoRutter <92889523+ChristoRutter@users.noreply.github.com>
2021-12-23 10:01:19 -0500

    getting info from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  useEffect(()=>{
+      axios.get('http://localhost:8000/api/catBreed')
+      .then((res)=>{
+        console.log(res.data)
+      })
+      .catch((err)=>{console.log(err)})
+  })
   return (
     <div className="App">
-      
+     
     </div>
   );
 }
